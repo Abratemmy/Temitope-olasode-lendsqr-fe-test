99da2c0c720ae48d31344d4b02f07bc26389d20b
temitope adeniyi <temitopeopeyemi2016@gmail.com>
2022-10-13 15:05:00 +0100

    Updates

---
+++ precache-manifest.7ee25f2ed977083b957d0d763f262868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1668b628a760c27b3dbb",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/css/main.8720a04d.chunk.css"
+  },
+  {
+    "revision": "1668b628a760c27b3dbb",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/js/main.1668b628.chunk.js"
+  },
+  {
+    "revision": "c04e2171de7893cf95d6",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/js/1.c04e2171.chunk.js"
+  },
+  {
+    "revision": "3cb8f9fe7041e29c3cdf",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/js/2.3cb8f9fe.chunk.js"
+  },
+  {
+    "revision": "c5b49ad5b16d242034c7",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/js/runtime~main.c5b49ad5.js"
+  },
+  {
+    "revision": "e0f52d4d40a0eddc0bdf20368c4e475f",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/media/Union.e0f52d4d.png"
+  },
+  {
+    "revision": "e8dad050d121beb8e7788f7e4ee74787",
+    "url": "/Temitope-olasode-lendsqr-fe-test/static/media/pablo-sign-in.e8dad050.svg"
+  },
+  {
+    "revision": "bb71535f783e8caeedbfff3e89e6652f",
+    "url": "/Temitope-olasode-lendsqr-fe-test/index.html"
+  }
+];
